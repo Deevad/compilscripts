1ebfc945f4f653d1756363aa5301f74ad48ac212
David Revoy <info@davidrevoy.com>
2013-04-01 16:16:58 +0200

    total reformating of the script, color, libs , etc...

--- vc-install.sh
+++ vc-install.sh
@@ -44,7 +44,6 @@ _separators()
 _setup_dir()
 {
 	mkdir -p $srcDir
-	mkdir -p $instDir
 	mkdir -p $buildDir
 }
 
@@ -57,34 +56,69 @@ _done()
 
 _install_dependencies()
 {
-	echo "This part will install all the dependencies for building $project"
-	echo "the system will be updated and new package installed"
+	echo "${RED} This part will install all the dependencies for building $project"
+	echo "the system will be updated and new package installed${BLACK}"
 	echo -n "press [Enter] to continue, or [Ctrl+C] to abort"
 	read CHOICE
+	_separators
+	echo "${BLUE}  Now, updating your system ${BLACK}"
+	echo "      "
 	sudo apt-get -y update
 	sudo apt-get -y upgrade
+	_separators
+	echo "${BLUE}  Now, installing dependencie packages : build-essential git cmake  ${BLACK}"
+	echo "      "
 	sudo apt-get -y install build-essential git cmake
+	_separators
+	echo "${BLUE}  Now, checking if no package are broken  ${BLACK}"
+	echo "      "
 	sudo apt-get -f install
 }
 
 _get_sources()
 {
 	cd $directory/$project/
+	_separators
+	echo "${BLUE}  Now, updating $project source with GIT ${BLACK}"
 	git clone $gitRepo src
 }
 
 _compile_sources()
 {
 	cd $buildDir
+	_separators
+	echo "${BLUE}  Now, configuring, compiling, installing ... this is the main part of the script, error are important to read ${BLACK}"
+	echo "${BLUE}  [ Note: this part can take from 2min to 30min depending of your CPU and disk speed access ] ${BLACK}"
+	echo "      "
 	cmake ../src
 	make -j$numCores
+	_separators
+	echo "${BLUE}  Now, installing $project direcly into your system ${BLACK}"
+	echo "      "
 	sudo make install -j$numCores
 }
 
 _update_sources()
 {
 	cd $srcDir
+	_separators
+	echo "${BLUE}  Now, updating $project source with GIT ${BLACK}"
 	git pull
+}
+
+_user_reset_master()
+{
+	echo "${BLUE}------------------------------------------------------"
+	echo "RESET TO MASTER"
+	echo "------------------------------------------------------${BLACK}"
+	echo "      "
+	cd $srcDir
+	git reset --hard master
+	git checkout master
+	git pull
+	git clean -dfx
+	_compile_sources
+	_done
 }
 
 _user_install()
@@ -101,6 +135,34 @@ _user_install()
 	_done
 }
 
+_user_uninstall()
+{
+	echo "${BLUE}------------------------------------------------------"
+	echo "UNINSTALLATION"
+	echo "------------------------------------------------------${BLACK}"
+	echo "      "
+	echo "${RED}This part will uninstall all $project"
+	echo "Obviously : to uninstall , you need a previous installation"
+	echo -n "press [Enter] to continue, or [Ctrl+C] to exit${BLACK}"
+	read CHOICE
+	cd $buildDir
+	make uninstall
+	echo "${RED} To-do manually : $project"
+	echo "* Now if you want to also delete the sources and build directory , delete manually $directory/$project ${BLACK} "
+	_done
+	
+}
+
+_user_compile_only()
+{
+	echo "${BLUE}------------------------------------------------------"
+	echo "COMPILE ONLY"
+	echo "------------------------------------------------------${BLACK}"
+	echo "      "
+	_compile_sources
+	_done
+}
+
 _user_update()
 {
 	echo "${BLUE}------------------------------------------------------"
@@ -112,56 +174,76 @@ _user_update()
 	_done
 }
 
+_endkey()
+{
+# a key to exit
+echo -n "Press [enter] to exit"
+read END
+}
+
 #######
 # UI  #
 #######
 
 clear
-tput rev
-echo "$project-install.sh"
-tput sgr0
+echo "${PINK}      "
+echo "    _ "
+echo " \/(_ "
 echo "      "
-echo "     __  (\_"
-echo "    (_ \ ( '>"
-echo "      ) \/_)="
-echo "      (_(_ )_"
+echo "${BLACK}     "
+echo " Welcome, this script will help you to compile and update $project"
 echo "      "
-echo "Compile, update, and more for $project"
-echo "  in "$directory/$project
-echo "  written for Linux Mint KDE 14"
-echo "      "
+echo "${GREEN}  *Infos*${BLACK} "
+echo "  - script version : "$scriptversion
+echo "  - source path :    "$srcDir
+echo "  - build path :    "$buildDir
+echo "  - install path :     " in your system ( make install )
+echo "  "
 # menu
-echo "------------------------------------------------------"
-echo "     MENU   "
+echo "${BLUE}------------------------------------------------------------${BLACK}"
+echo "${BLUE}     MENU   ${BLACK}"
 echo "      "
-echo "   1) Install"
-echo "   2) Update"
-echo "   3) Open sources folder"
-echo "   4) Help webpage"
-echo "   ... or Ctrl+C to exit"
+echo "   (1) Install"
+echo "   (2) Update"
+echo "   (3) Compile only"
+echo "   (4) Reset to master"
+echo "   (5) Uninstall"
+echo "   (6) Online manual "
+echo "   (7) Exit"
 echo " "
-echo "------------------------------------------------------"
-echo -n "                  Enter your choice (1-4) :"
+echo "${BLUE}------------------------------------------------------------${BLACK}"
+echo -n "               Enter your choice (1-7) then press [enter] :${PINK}"
 read mainmenu
 echo " "
-echo " "
+echo " ${BLACK}"
+clear
+
 	if [ "$mainmenu" = 1 ]; then
 		_user_install
+		_endkey
 		
 	elif [ "$mainmenu" = 2 ]; then
 		_user_update
-
-	elif [ "$mainmenu" = 3 ]; then
-		cd $srcDir
+		_endkey
 		
+	elif [ "$mainmenu" = 3 ]; then
+		_user_compile_only
+		_endkey
+
 	elif [ "$mainmenu" = 4 ]; then
+		_user_reset_master
+		_endkey
+		
+	elif [ "$mainmenu" = 5 ]; then
+		_user_uninstall
+		_endkey
+		
+	elif [ "$mainmenu" = 6 ]; then
 		xdg-open $helppage
+	
+	elif [ "$mainmenu" = 7 ]; then
+		echo " ${PINK}Bye Bye ! ${BLACK} "
+		
 	else
 	echo "the script couldn't understand your choice, try again...";
 	fi;
-	
-cd $directory
-
-# a key to prevent terminal closing
-echo -n "Script finished job, press a key to exit"
-read END
